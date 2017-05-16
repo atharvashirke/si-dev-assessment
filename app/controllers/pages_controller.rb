3fe3fdfd2b42a1b35fe762eb3bf0a034a1bc1212
Atharva Shirke <atharvashirke2018@gmail.com>
2017-05-16 08:49:00 +0000

    Finishing Task 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,18 @@ class PagesController < ApplicationController
   end
 
   def sms
-    
+    require 'twilio-ruby'
+
+# To find these visit https://www.twilio.com/user/account
+account_sid = "AC0d52b0bf62a8ceecf0a1b475f18f36c1"
+auth_token = "6ea57f2d6f12d6addc2836ed38119946"
+
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+@message = @client.messages.create(
+  to: params[:phone],
+  from: "+19259058206" ,
+  body: params[:body],
+)
   end
 end
